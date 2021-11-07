aa49de0281bfd204a814aa7eaa773ffb1a0bd6f6
Alex <sansemi@gmail.com>
2021-11-07 21:01:56 +0100

    du vom master

--- src/main/java/com/packagename/myapp/AnalyseView.java
+++ src/main/java/com/packagename/myapp/AnalyseView.java
@@ -1,5 +1,6 @@
 package com.packagename.myapp;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -9,6 +10,12 @@ public class AnalyseView extends VerticalLayout {
 
     public AnalyseView() {
         add();
+
+        Button button = new Button("Greet");
+        button.addClickListener(e ->
+                button.getUI().ifPresent(ui ->
+                        ui.navigate("greeting")));
+        add(button);
     }
 
     public String privet() {
