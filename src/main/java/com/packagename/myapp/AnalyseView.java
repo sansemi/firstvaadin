8eb7a3630d6e02f502f45ccffc9f449b98d8e639
Alex <sansemi@gmail.com>
2021-11-07 20:59:55 +0100

    du vom master

--- src/main/java/com/packagename/myapp/AnalyseView.java
+++ src/main/java/com/packagename/myapp/AnalyseView.java
@@ -1,27 +1,20 @@
 package com.packagename.myapp;
 
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import com.vaadin.ui.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Route("calc")
 public class AnalyseView extends VerticalLayout {
 
     public AnalyseView() {
-
         add();
-
     }
 
     public String privet() {
-        String otvet = "Hallo du!";
+        String  antwort = "Hallo du!";
         new Span();
-        return otvet;
+        return antwort;
     }
 
 }
