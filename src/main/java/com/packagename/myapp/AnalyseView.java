09f9ad96782fdd218e8410804c08103c213fc027
Alex <sansemi@gmail.com>
2021-11-21 08:26:45 +0100

    start hotfix

--- src/main/java/com/packagename/myapp/AnalyseView.java
+++ src/main/java/com/packagename/myapp/AnalyseView.java
@@ -26,7 +26,8 @@ public class AnalyseView extends VerticalLayout {
         String  antwort1 = "Hallo lieber du!";
         String  antwort2 = "Hallo lieber du!";
         String  antwort3 = "Hallo lieber du!";
-        String  antwort4= "Hallo lieber du!";
+        String  antwort5= "Hallo lieber du!";
+
 
 
         new Span();
